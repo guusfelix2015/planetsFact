7d0fbdb016c8ec861d7a13708844d9ea1091455e
guusfelix2015 <gusfelix2015@gmail.com>
2022-07-20 00:01:28 -0300

    Finish Project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
+import { useState } from "react";
 import styles from "./Header.module.css";
 
 const Header = () => {
   const url = "http://localhost:3000/planets";
   const { data: planets } = useFetch(url);
+
+  const [navActive, setNavActive] = useState(styles.links);
+  const [toggleIcon, setToggleIcon] = useState(styles.navToggle);
+
+  const navToggle = () => {
+    navActive === styles.links
+      ? setNavActive(`${styles.links} ${styles.navActive} `)
+      : setNavActive(styles.links);
+
+    toggleIcon === styles.navToggle
+      ? setToggleIcon(`${styles.navToggle} ${styles.toggle}`)
+      : setToggleIcon(styles.navToggle);
+  };
 
   return (
     <header>
@@ -12,14 +26,24 @@ const Header = () => {
         <Link className={styles.logo} to="/">
           The Planets
         </Link>
-        <ul className={styles.links}>
+        <ul className={navActive}>
           {planets &&
             planets.map((planet) => (
               <li key={planet.id}>
-                <Link to={`/planets/${planet.name}`}>{planet.name}</Link>
+                <Link
+                  onClick={() => setNavActive(styles.links)}
+                  to={`/planets/${planet.name}`}
+                >
+                  {planet.name}
+                </Link>
               </li>
             ))}
         </ul>
+        <div onClick={navToggle} className={toggleIcon}>
+          <div className={styles.line1}></div>
+          <div className={styles.line2}></div>
+          <div className={styles.line3}></div>
+        </div>
       </nav>
     </header>
   );
