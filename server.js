34f663901b32124304a405fb515cba2f5d2d811d
guusfelix2015 <gusfelix2015@gmail.com>
2022-07-20 01:41:02 -0300

    --amend

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("data/db.json");
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router("data/dbcopy.json");
+const middlewares = jsonServer.defaults({ static: "./assets" });
 
 server.use(middlewares);
 server.use(router);
